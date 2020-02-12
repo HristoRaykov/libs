4e4d40421118f8299f10157cac8f39c9a0b6ee2c
hristocr <hristocr@gmail.com>
2020-02-12 19:14:29 +0200

    ...

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-from ts_train_test_split.train_test_split import split_time_series_by_time_steps
+from ts_train_test_split.train_test_split import split_time_series_by_time_steps_index, \
+	split_time_series_by_time_steps_column
 from ts_transformer.pipeline import create_pipeline
 
 LAG_COLS = ['adj_close', 'volume', 'adj_open', 'adj_low',
@@ -28,12 +29,13 @@ LAG_DIFF_SUFFIX_KW = "_lag_diff_"
 stocks = pd.read_csv("/home/hristocr/Desktop/dataset.csv")
 stocks["date"] = stocks["date"].astype(np.datetime64)
 
-pipeline = create_pipeline(group_col=GROUP_COL, date_col=DATE_COL, target_col=TARGET_COL,
-                           create_lags=True, lag_cols=LAG_COLS, lag_num=5, drop_lag_col=True,
-                           trans_index=True,
-                           create_ind_vars=True,
-                           time_step_trans=True)
+# pipeline = create_pipeline(group_col=GROUP_COL, date_col=DATE_COL, target_col=TARGET_COL,
+#                            create_lags=True, lag_cols=LAG_COLS, lag_num=5, drop_lag_col=True,
+#                            trans_index=True,
+#                            create_ind_vars=True,
+#                            time_step_trans=True)
+#
+# df = pipeline.fit_transform(stocks)
 
-df = pipeline.fit_transform(stocks)
+# train, test = split_time_series_by_time_steps_column(stocks, time_step_col="date", n_time_steps=5)
 
-train, test = split_time_series_by_time_steps(df, n_time_steps=5)
